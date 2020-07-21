ca3d857cdcbfe033c28dadbead919e5d14417e3f
Matthew <mattk.development@gmail.com>
2020-07-21 13:42:35 -0500

    Task 1&2 Complete

--- index.js
+++ index.js
@@ -4,22 +4,65 @@ console.log(fifaData);
 console.log('its working');
 // ⚽️ M  V P ⚽️ //
 
-/* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
+
+/* 
+{
+    "Year": 1930,
+    "Datetime": "13 Jul 1930 - 15:00",
+    "Stage": "Group 1",
+    "Stadium": "Pocitos",
+    "City": "Montevideo",
+    "Home Team Name": "France",
+    "Home Team Goals": 4,
+    "Away Team Goals": 1,
+    "Away Team Name": "Mexico",
+    "Win conditions": "",
+    "Attendance": 4444,
+    "Half-time Home Goals": 3,
+    "Half-time Away Goals": 0,
+    "Referee": "LOMBARDI Domingo (URU)",
+    "Assistant 1": "CRISTOPHE Henry (BEL)",
+    "Assistant 2": "REGO Gilberto (BRA)",
+    "RoundID": 201,
+    "MatchID": 1096,
+    "Home Team Initials": "FRA",
+    "Away Team Initials": "MEX"
+  }
+
+Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
 
 (a) Home Team name for 2014 world cup final
 (b) Away Team name for 2014 world cup final
 (c) Home Team goals for 2014 world cup final
 (d) Away Team goals for 2014 world cup final
 (e) Winner of 2014 world cup final */
+const twentyFourteenFinals = fifaData.filter(function(game){
+    return game.Stage === 'Final' && game.Year === 2014;
+});
+console.log(twentyFourteenFinals[0]['Home Team Name']);
+console.log(twentyFourteenFinals[0]['Away Team Name']);
+console.log(twentyFourteenFinals[0]['Home Team Goals']);
+console.log(twentyFourteenFinals[0]['Away Team Goals']);
+console.log(twentyFourteenFinals[0]['Win conditions']);
+
+
+
 
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
+function getFinals(data){
+    return data.filter(function(item){
+        return item.Stage === 'Final'
+    })
+}
+console.log(getFinals(fifaData));
 
-    /* code here */
 
-};
+
+
+
+
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
@@ -30,6 +73,10 @@ function getYears(/* code here */) {
 };
 
 getYears();
+
+
+
+
 
 /* Task 5: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
